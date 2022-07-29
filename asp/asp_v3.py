4906f5f92c778a6b456b79e42ce0e88a5b9e6fc4
Trung Le <le.hg.trung@gmail.com>
2022-07-29 04:43:57 -0600

    test on bk

--- asp/asp_v3.py
+++ asp/asp_v3.py
@@ -235,8 +235,8 @@ def curriculum_training(labeled_path,
 
 
 if __name__ == '__main__':
-    LABELED_PATH = '../datasets/unified/train.CoNLL04_30_labeled.json'
-    UNLABELED_PATH = '../datasets/unified/train.CoNLL04_30_unlabeled.json'
+    LABELED_PATH = '../datasets/unified/train.CoNLL04_30_labeled.bk.json'
+    UNLABELED_PATH = '../datasets/unified/train.CoNLL04_30_unlabeled.bk.json'
     RAW_PSEUDO_LABELED_PATH = '../datasets/pseudo/raw.CoNLL04_30.json'
     SELECTED_PSEUDO_LABELED_PATH = '../datasets/pseudo/selected.CoNLL04_30.json'
     LABELED_MODEL_PATH = '../ckpts/pseudo/labeled/labeled'
