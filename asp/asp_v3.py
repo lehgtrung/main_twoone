fa94f4e5ef9b9f38208e6fa0835e351504b2e24a
Trung Le <le.hg.trung@gmail.com>
2022-07-29 04:47:17 -0600

    test on bk

--- asp/asp_v3.py
+++ asp/asp_v3.py
@@ -13,7 +13,7 @@ def conll04_script():
         --evaluate_interval 500 \
         --dataset CoNLL04 \
         --pretrained_wv ../wv/glove.6B.100d.conll04.txt \
-        --max_epoches 2000 \
+        --max_epoches 1 \
         --max_steps 30000 \
         --model_class JointModel \
         --crf None  \
