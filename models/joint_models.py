f2c17ee1aec09709b5128f71fb2d537cf4e6c348
Trung Le <le.hg.trung@gmail.com>
2022-08-22 05:50:56 -0600

    measure time

--- models/joint_models.py
+++ models/joint_models.py
@@ -328,26 +328,15 @@ class JointModel(Tagger):
             'masks': Tensor
         }
         '''
-        #print('_ner_tags.shape: ', inputs['_ner_tags'].shape)
-        #print('_re_tags.shape: ', inputs['_re_tags'].shape)
-        tic = time.time()
         inputs = self.forward_embeddings(inputs)
-        toc = time.time()
-        print('forward time: ', toc - tic)
         tab_embeddings = inputs['tab_embeddings']
         seq_embeddings = inputs['seq_embeddings']
 
-        tic = time.time()
         re_tag_logits = self.re_tag_logits_layer(tab_embeddings)
-        toc = time.time()
-        #print('re_tag_logits time: ', toc - tic)
         
         # use diagonal elements
         #ner_tag_embeddings = relation_embeddings.diagonal(dim1=1, dim2=2).permute(0, -1, 1)
-        tic = time.time()
         ner_tag_logits = self.ner_tag_logits_layer(seq_embeddings)
-        toc = time.time()
-        #print('re_tag_logits time: ', toc - tic)
         
         rets = inputs
         rets['ner_tag_logits'] = ner_tag_logits
