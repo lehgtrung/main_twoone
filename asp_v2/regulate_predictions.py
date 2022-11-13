a422a5289de3b2b7559942ad6c8ee0d2776ed51c
Trung Le <le.hg.trung@gmail.com>
2022-11-13 02:35:22 -0700

    finish adding multiple models instead of 2

--- asp_v2/regulate_predictions.py
+++ asp_v2/regulate_predictions.py
@@ -8,16 +8,17 @@ from tqdm import tqdm
 import numpy as np
 
 
-atomed_output_path = 'asp_v2/v5/atomed_preds/{model_number}/{sent_number}.txt'
-answerset_output_path = 'asp_v2/v5/answersets/{model_number}/{sent_number}.txt'
-selected_path = 'asp_v2/v5/selected_answersets/{model_number1}.{model_number2}/{sent_number}.txt'
+atomed_output_path = 'asp_v2/v5/atomed_preds/{iter_number}/{model_number}/{sent_number}.txt'
+answerset_output_path = 'asp_v2/v5/answersets/{iter_number}/{model_number}/{sent_number}.txt'
+selected_path = 'asp_v2/v5/selected_answersets/{iter_number}/{combined_model_numbers}/{sent_number}.txt'
 command = 'clingo --opt-mode=optN asp_v2/v5/p5.lp ' + atomed_output_path + \
           ' --outf=0 -V0 --out-atomf=%s. --quiet=1,2,2'
 
 
-def solve(model_number, sent_number):
+def solve(model_number, iter_number, sent_number):
     # Write the program to a file
     process = subprocess.Popen(command.format(model_number=model_number,
+                                              iter_number=iter_number,
                                               sent_number=sent_number).split(),
                                stdin=subprocess.PIPE,
                                stderr=subprocess.PIPE,
@@ -49,13 +50,17 @@ def write_down_a_list(path, lst):
         f.writelines(map(lambda x: x + '\n', lst))
 
 
-def convert_to_consistent_answersets(preds_path, model_number):
-    os.makedirs(os.path.dirname(atomed_output_path.format(model_number=model_number,
-                                                          sent_number=0)),
-                exist_ok=True)
-    os.makedirs(os.path.dirname(answerset_output_path.format(model_number=model_number,
-                                                             sent_number=0)),
-                exist_ok=True)
+def convert_to_consistent_answersets(preds_path, iter_number, model_number):
+    os.makedirs(os.path.dirname(atomed_output_path.format(
+            iter_number=iter_number,
+            model_number=model_number,
+            sent_number=0)),
+        exist_ok=True)
+    os.makedirs(os.path.dirname(answerset_output_path.format(
+            iter_number=iter_number,
+            model_number=model_number,
+            sent_number=0)),
+        exist_ok=True)
     # Load the predictions
     with open(preds_path, 'r') as f:
         preds = json.load(f)
@@ -63,18 +68,18 @@ def convert_to_consistent_answersets(preds_path, model_number):
     for i, row in enumerate(preds):
         atoms = convert_to_atoms(row, prefix='atom')
         # Path to write the atomed preds down
-        path = atomed_output_path.format(model_number=model_number,
+        path = atomed_output_path.format(iter_number=iter_number,
+                                         model_number=model_number,
                                          sent_number=i)
         write_down_a_list(path, atoms)
     # Convert the atomed preds to answersets
     for i in tqdm(range(len(preds))):
-        path = answerset_output_path.format(model_number=model_number,
+        path = answerset_output_path.format(iter_number=iter_number,
+                                            model_number=model_number,
                                             sent_number=i)
-        answersets = solve(model_number=model_number, sent_number=i)
-        # with open(path, 'w') as f:
-        #     json.dump(answersets, f)
-        # if len(answersets) > 1:
-        #     print(i)
+        answersets = solve(model_number=model_number,
+                           iter_number=iter_number,
+                           sent_number=i)
         with open(path, 'w') as f:
             for answerset in answersets:
                 f.writelines(map(lambda x: x+'\n', answerset))
@@ -102,63 +107,66 @@ def compute_set_diff(answerset1, answerset2):
     return n + m - 2*len(intersect)
 
 
-def create_dist_graph(answersets1, answersets2, self=False):
+def create_dist_graph(all_answersets, self=False):
     graph = nx.Graph()
-    for i, answerset1 in enumerate(answersets1):
-        for j, answerset2 in enumerate(answersets2):
-            weight = compute_set_diff(answerset1, answerset2)
-            graph.add_edge(f'left.{i}', f'right.{j}', weight=weight)
+
+    for i, answersets in enumerate(all_answersets):
+        for j, other_answersets in enumerate(all_answersets):
+            if j > i:
+                for k, answerset in enumerate(answersets):
+                    for g, other_answerset in enumerate(other_answersets):
+                        weight = compute_set_diff(answerset, other_answerset)
+                        graph.add_edge(f'{i}.{k}', f'{j}.{g}', weight=weight)
     if self:
-        for i in range(len(answersets1)):
-            for j in range(len(answersets1)):
-                if i < j:
-                    weight = compute_set_diff(answersets1[i], answersets1[j])
-                    graph.add_edge(f'left.{i}', f'left.{j}', weight=weight)
-        for i in range(len(answersets2)):
-            for j in range(len(answersets2)):
-                if i < j:
-                    weight = compute_set_diff(answersets2[i], answersets2[j])
-                    graph.add_edge(f'right.{i}', f'right.{j}', weight=weight)
+        for i, answersets in enumerate(all_answersets):
+            for j in range(len(answersets)):
+                for k in range(len(answersets)):
+                    if k > j:
+                        weight = compute_set_diff(answersets[i], answersets[j])
+                        graph.add_edge(f'{i}.{j}', f'{i}.{k}', weight=weight)
     return graph
 
 
-def find_center_vertex(model_number1, model_number2, sent_number):
-    # Read 2 files, parse them and create a graph
-    path = answerset_output_path.format(model_number=model_number1,
-                                        sent_number=sent_number)
-    answersets0 = parse_answersets_from_file(path, with_break=True)
-    path = answerset_output_path.format(model_number=model_number2,
-                                        sent_number=sent_number)
-    answersets1 = parse_answersets_from_file(path, with_break=True)
-    graph = create_dist_graph(answersets0, answersets1, self=False)
+def find_center_vertex(sent_number, iter_number, model_numbers):
+    all_answersets = []
+    for model_number in model_numbers:
+        path = answerset_output_path.format(iter_number=iter_number,
+                                            model_number=model_number,
+                                            sent_number=sent_number)
+        answersets = parse_answersets_from_file(path, with_break=True)
+        all_answersets.append(answersets)
+    graph = create_dist_graph(all_answersets)
     centrality = nx.closeness_centrality(graph)
     center = max(centrality, key=centrality.get)
     part, index = center.split('.')
+    part = int(part)
     index = int(index)
-    if part == 'left':
-        return answersets0[index]
-    return answersets1[index]
+    return all_answersets[part][index]
 
 
-def select_answerset(model_number1, model_number2):
-    os.makedirs(os.path.dirname(selected_path.format(model_number1=model_number1,
-                                                     model_number2=model_number2,
+def select_answerset(iter_number, model_numbers):
+    model_numbers = sorted(list(model_numbers))
+    combined_model_numbers = '.'.join(map(str, model_numbers))
+    os.makedirs(os.path.dirname(selected_path.format(iter_number=iter_number,
+                                                     combined_model_numbers=combined_model_numbers,
                                                      sent_number=0)),
                 exist_ok=True)
-    n = len(os.listdir(os.path.dirname(answerset_output_path.format(model_number=model_number1,
+    n = len(os.listdir(os.path.dirname(answerset_output_path.format(iter_number=iter_number,
+                                                                    model_number=model_numbers[0],
                                                                     sent_number=0))))
     # For each sentence find the answerset that minimize avg set diff
     for i in tqdm(range(n)):
-        path = selected_path.format(model_number1=model_number1,
-                                    model_number2=model_number2,
+        path = selected_path.format(iter_number=iter_number,
+                                    combined_model_numbers=combined_model_numbers,
                                     sent_number=i)
-        selected_answerset = find_center_vertex(model_number1=model_number1,
-                                                model_number2=model_number2,
-                                                sent_number=i)
+        selected_answerset = find_center_vertex(sent_number=i,
+                                                iter_number=iter_number,
+                                                model_numbers=model_numbers)
         write_down_a_list(path, selected_answerset)
 
 
-def compare_with_gt(gt_path, model_number1, model_number2):
+def compare_with_gt(gt_path, iter_number, model_numbers):
+    model_numbers = sorted(list(model_numbers))
     with open(gt_path, 'r') as f:
         gt = json.load(f)
     all_gt_atoms = []
@@ -167,8 +175,9 @@ def compare_with_gt(gt_path, model_number1, model_number2):
         all_gt_atoms.append(gt_atoms)
     total_set_diff = []
     for i in range(len(all_gt_atoms)):
-        path = selected_path.format(model_number1=model_number1,
-                                    model_number2=model_number2,
+        combined_model_numbers = '.'.join(map(str, model_numbers))
+        path = selected_path.format(iter_number=iter_number,
+                                    combined_model_numbers=combined_model_numbers,
                                     sent_number=i)
         answerset = parse_answersets_from_file(path, with_break=False)
         set_diff = compute_set_diff(all_gt_atoms[i], answerset)
@@ -199,51 +208,32 @@ def compare_raw_with_gt(raw_path, gt_path):
 
 
 if __name__ == '__main__':
-    # TODO:
-    # Combine 3 models instead of 2 models
     _iter = 1
-    _model_number = 0
-    convert_to_consistent_answersets(f'asp_v2/v5/preds/iter={_iter}/prediction_{_model_number}.json',
-                                     _model_number)
-    _model_number = 1
-    convert_to_consistent_answersets(f'asp_v2/v5/preds/iter={_iter}/prediction_{_model_number}.json',
-                                     _model_number)
-    select_answerset(model_number1=0,
-                     model_number2=1)
-
-    _model_number = 0
-    convert_to_consistent_answersets(f'asp_v2/v5/preds/iter={_iter}/prediction_{_model_number}.json',
-                                     _model_number)
-    _model_number = 2
-    convert_to_consistent_answersets(f'asp_v2/v5/preds/iter={_iter}/prediction_{_model_number}.json',
-                                     _model_number)
-    select_answerset(model_number1=0,
-                     model_number2=2)
-
-    _model_number = 1
-    convert_to_consistent_answersets(f'asp_v2/v5/preds/iter={_iter}/prediction_{_model_number}.json',
-                                     _model_number)
-    _model_number = 2
-    convert_to_consistent_answersets(f'asp_v2/v5/preds/iter={_iter}/prediction_{_model_number}.json',
-                                     _model_number)
-    select_answerset(model_number1=1,
-                     model_number2=2)
+    for _model_number in range(3):
+        convert_to_consistent_answersets(f'asp_v2/v5/preds/iter={_iter}/prediction_{_model_number}.json',
+                                         iter_number=_iter,
+                                         model_number=_model_number)
+    select_answerset(_iter, [0, 1])
+    select_answerset(_iter, [0, 2])
+    select_answerset(_iter, [1, 2])
+    select_answerset(_iter, [0, 1, 2])
 
     print('Compare joint M0,M1 with gt')
     compare_with_gt('datasets/core_conll04/conll04_30/fold=1/unlabeled.json',
-                    model_number1=0,
-                    model_number2=1)
+                    _iter, [0, 1])
     print('===================================')
     print('Compare joint M0,M2 with gt')
     compare_with_gt('datasets/core_conll04/conll04_30/fold=1/unlabeled.json',
-                    model_number1=0,
-                    model_number2=2)
+                    _iter, [0, 2])
     print('===================================')
     print('Compare joint M1,M2 with gt')
     compare_with_gt('datasets/core_conll04/conll04_30/fold=1/unlabeled.json',
-                    model_number1=1,
-                    model_number2=2)
+                    _iter, [1, 2])
     print('===================================')
+    print('Compare joint M0,M1,M2 with gt')
+    compare_with_gt('datasets/core_conll04/conll04_30/fold=1/unlabeled.json',
+                    _iter, [0, 1, 2])
+    print('***********************************')
     print('Compare M0 with gt')
     compare_raw_with_gt(raw_path=f'asp_v2/v5/preds/iter={_iter}/prediction_0.json',
                         gt_path='datasets/core_conll04/conll04_30/fold=1/unlabeled.json')
