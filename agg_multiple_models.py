cbe50d874bc330873fea17c6bf432bd99dc589e5
Trung Le <le.hg.trung@gmail.com>
2022-11-16 08:16:24 -0700

    minor

--- agg_multiple_models.py
+++ agg_multiple_models.py
@@ -126,6 +126,7 @@ def aggregate_multiple_models(inputs, models):
             print(torch.norm(ten))
         for ten in lst_re_tag_logits:
             print(torch.norm(ten))
+        exit()
 
         ner_tag_logits = torch.mean(torch.stack(lst_ner_tag_logits), dim=0)
         re_tag_logits = torch.mean(torch.stack(lst_re_tag_logits), dim=0)
@@ -177,7 +178,7 @@ if __name__ == '__main__':
     model_paths = [
         # 'datasets/methods/tri_training/conll04_30/fold=1/models/labeled_0',
         'datasets/methods/tri_training/conll04_30/fold=1/models/labeled_1',
-        # 'datasets/methods/tri_training/conll04_30/fold=1/models/labeled_2'
+        'datasets/methods/tri_training/conll04_30/fold=1/models/labeled_2'
     ]
     DEFAULT_TEST_PATH = './datasets/core_conll04/test.conll04.json'
     DEFAULT_VALID_PATH = './datasets/core_conll04/valid.conll04.json'
