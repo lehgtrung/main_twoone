e26e17b123a05ee344ff0b87677881b277ac895b
Trung Le <le.hg.trung@gmail.com>
2022-11-17 19:02:33 -0700

    done

--- asp_utils.py
+++ asp_utils.py
@@ -7,6 +7,7 @@ import glob
 import random
 import re
 import torch
+import copy
 
 
 atomed_output_path = 'methods/tri_training_with_asp/atomed_preds/{iter_number}/{model_number}/{sent_number}.txt'
@@ -253,8 +254,8 @@ def select_agreement_with_asp(iter_number, model_number1, model_number2,
     # Convert selected sentences
     gts = [unlabeled_data[i] for i in selected_indices]
     preds = convert_atoms_to_file_form(tokens_list, selected_answersets)
-    print(preds[:10])
-    evaluate_model(preds, gts, logger)
+    for_eval_preds = copy.copy(preds)
+    evaluate_model(for_eval_preds, gts, logger)
     with open(out_path, 'w') as f:
         json.dump(preds, f)
     agree_ratio = len(selected_indices) / len(gts)
