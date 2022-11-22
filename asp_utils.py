f464091ef23c864b6e4f5e9ffb5967ec3c550541
Trung Le <le.hg.trung@gmail.com>
2022-11-22 02:27:01 -0700

    remove relations with ner

--- asp_utils.py
+++ asp_utils.py
@@ -181,23 +181,23 @@ def evaluate_model(preds, gts, logger=None):
             gt['entities'] = [gt['entities']]
             pred['relations'] = [set(tuple(e) for e in pred['relations'])]
             gt['relations'] = [gt['relations']]
-            pred_span_to_etype = [{(ib, ie): etype for ib, ie, etype in x} for x in pred['entities']]
-            label_span_to_etype = [{(ib, ie): etype for ib, ie, etype in x} for x in gt['entities']]
+            # pred_span_to_etype = [{(ib, ie): etype for ib, ie, etype in x} for x in pred['entities']]
+            # label_span_to_etype = [{(ib, ie): etype for ib, ie, etype in x} for x in gt['entities']]
             pred_entities += pred['entities']
             label_entities += gt['entities']
             pred_relations += pred['relations']
             label_relations += gt['relations']
 
-            pred_relations_wNER += [
-                [
-                    (ib, ie, m[(ib, ie)], jb, je, m[(jb, je)], rtype) for ib, ie, jb, je, rtype in x
-                ] for x, m in zip(pred['relations'], pred_span_to_etype)
-            ]
-            label_relations_wNER += [
-                [
-                    (ib, ie, m[(ib, ie)], jb, je, m[(jb, je)], rtype) for ib, ie, jb, je, rtype in x
-                ] for x, m in zip(gt['relations'], label_span_to_etype)
-            ]
+            # pred_relations_wNER += [
+            #     [
+            #         (ib, ie, m[(ib, ie)], jb, je, m[(jb, je)], rtype) for ib, ie, jb, je, rtype in x
+            #     ] for x, m in zip(pred['relations'], pred_span_to_etype)
+            # ]
+            # label_relations_wNER += [
+            #     [
+            #         (ib, ie, m[(ib, ie)], jb, je, m[(jb, je)], rtype) for ib, ie, jb, je, rtype in x
+            #     ] for x, m in zip(gt['relations'], label_span_to_etype)
+            # ]
 
             sents += [pred['tokens']]
 
@@ -206,21 +206,21 @@ def evaluate_model(preds, gts, logger=None):
             sents, pred_entities, label_entities)
         rets['relation_p'], rets['relation_r'], rets['relation_f1'] = get_metrics(
             sents, pred_relations, label_relations)
-        rets['relation_p_wNER'], rets['relation_r_wNER'], rets['relation_f1_wNER'] = get_metrics(
-            sents, pred_relations_wNER, label_relations_wNER)
+        # rets['relation_p_wNER'], rets['relation_r_wNER'], rets['relation_f1_wNER'] = get_metrics(
+        #     sents, pred_relations_wNER, label_relations_wNER)
 
     e_precision, e_recall, e_f1 = rets['entity_p'], rets['entity_r'], rets['entity_f1']
     print(f">> entity prec:{e_precision:.4f}, rec:{e_recall:.4f}, f1:{e_f1:.4f}")
     r_precision, r_recall, r_f1 = rets['relation_p'], rets['relation_r'], rets['relation_f1']
     print(f">> relation prec:{r_precision:.4f}, rec:{r_recall:.4f}, f1:{r_f1:.4f}")
-    rwe_precision, rwe_recall, rwe_f1 = rets['relation_p_wNER'], rets['relation_r_wNER'], rets['relation_f1_wNER']
-    print(f">> relation with NER prec:{rwe_precision:.4f}, rec:{rwe_recall:.4f}, f1:{rwe_f1:.4f}")
+    # rwe_precision, rwe_recall, rwe_f1 = rets['relation_p_wNER'], rets['relation_r_wNER'], rets['relation_f1_wNER']
+    # print(f">> relation with NER prec:{rwe_precision:.4f}, rec:{rwe_recall:.4f}, f1:{rwe_f1:.4f}")
 
     if logger:
         logger.info(
             f">> entity prec:{e_precision:.4f}, rec:{e_recall:.4f}, f1:{e_f1:.4f}\n"
             f">> relation prec:{r_precision:.4f}, rec:{r_recall:.4f}, f1:{r_f1:.4f}\n"
-            f">> relation prec:{rwe_precision:.4f}, rec:{rwe_recall:.4f}, f1:{rwe_f1:.4f}"
+            # f">> relation prec:{rwe_precision:.4f}, rec:{rwe_recall:.4f}, f1:{rwe_f1:.4f}"
         )
 
 
